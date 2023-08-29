80b5d7c08a73da0eeb7c6400b97cdea9601816a7
Vishal-sanap <sanapvishal28@gmail.com>
2023-08-29 18:35:21 +0530

    1

--- hotelmanagement/src/app/user/userlanding/userlanding.component.ts
+++ hotelmanagement/src/app/user/userlanding/userlanding.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { DataService } from 'src/app/data.service';
+// import { Router } from '@angular/router';
+// import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-userlanding',
@@ -8,43 +8,43 @@ import { DataService } from 'src/app/data.service';
   styleUrls: ['./userlanding.component.scss']
 })
 export class UserlandingComponent {
-  hideSignForm= false;
-  responseData : any;
-  signINForm: any;
-  signInForm: any;
+//   hideSignForm= false;
+//   responseData : any;
+//   signINForm: any;
+//   signInForm: any;
 
-  constructor(private dataService :DataService, private router : Router){}
+//   constructor(private dataService :DataService, private router : Router){}
   
 
-  async submit(data:any){
-    console.log(data,'data');
-    this.signInForm = data;
-    //console.log(this.signInForm,'this.signInForm');
+//   async submit(data:any){
+//     console.log(data,'data');
+//     this.signInForm = data;
+//     //console.log(this.signInForm,'this.signInForm');
     
-    this.responseData = await this.dataService.getApiCall().toPromise()
-    console.log(this.responseData ,"this.responseData ");
+//     this.responseData = await this.dataService.getApiCall().toPromise()
+//     console.log(this.responseData ,"this.responseData ");
       
-    if(this.responseData){
-      let journey = this.responseData.find((ele :any)=>{
-        return this.signInForm.userName == ele.userName && this.signInForm.password == ele.userpass
-      })
-      if(journey){
-        alert('Login Successful')
-        this.router.navigateByUrl('/user/usersuccess');
-      }
-      else{
-        alert('user not found')
-        this.router.navigateByUrl('/user/userland')
+//     if(this.responseData){
+//       let journey = this.responseData.find((ele :any)=>{
+//         return this.signInForm.userName == ele.userName && this.signInForm.password == ele.userpass
+//       })
+//       if(journey){
+//         alert('Login Successful')
+//         this.router.navigateByUrl('/user/usersuccess');
+//       }
+//       else{
+//         alert('user not found')
+//         this.router.navigateByUrl('/user/userland')
 
-      }
-    }
+//       }
+//     }
    
     
 
     
     
- }
- showSignForm(){
-  this.hideSignForm = true
- }
+//  }
+//  showSignForm(){
+//   this.hideSignForm = true
+//  }
 }
--- hotelmanagement/src/app/user/usersignin/usersignin.component.ts
+++ hotelmanagement/src/app/user/usersignin/usersignin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-usersignin',
@@ -6,16 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./usersignin.component.scss']
 })
 export class UsersigninComponent {
-  
- 
+  hideSignForm= false;
   responseData : any;
   signINForm: any;
   signInForm: any;
 
+  constructor(private dataService :DataService, private router : Router){}
   
 
   async submit(data:any){
     console.log(data,'data');
     this.signInForm = data;
+    //console.log(this.signInForm,'this.signInForm');
+    
+    this.responseData = await this.dataService.getApiCall().toPromise()
+    console.log(this.responseData ,"this.responseData ");
+      
+    if(this.responseData){
+      let journey = this.responseData.find((ele :any)=>{
+        return this.signInForm.userName == ele.userName && this.signInForm.password == ele.userpass
+      })
+      if(journey){
+        alert('Login Successful')
+        this.router.navigateByUrl('/user/usersuccess');
+      }
+      else{
+        alert('user not found')
+        this.router.navigateByUrl('/user/userland')
+
+      }
+    }
+   
+    
+
+    
+    
+ }
+ showSignForm(){
+  this.hideSignForm = true
+ }
 }
-}
+
